e3f22c351bef0c2d7a11ba6aadd5929668c89305
DESKTOP-7P5M41M\subfa <sabfarid@mail.ru>
2022-06-15 17:14:18 +0300

    first commit

--- App.js
+++ App.js
@@ -1,24 +1,38 @@
+//React Native Button
+//https://aboutreact.com/react-native-button/
+
+//import React in our code
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+//import all the components we are going to use
+import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
+
+const App = () => {
+  const onPressLearnMore = () => {
+    //For generating alert on buttton click
+    alert('Кнопка нажата');
+  }
+
   return (
-
-    <View style={styles.container}>
-      <Text category='h1'>Приложение OCPIO</Text>
-      <Text category='subtit'>Социальная сеть бронирования</Text>
-      <Text>Добро пожаловать!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      {/* enclose all components in this View tag */}
+      <View style={styles.container}>
+        <Text>Button Example</Text>
+        {/* Button whith handler function named onPressLearnMore*/}
+        <Button
+          onPress={onPressLearnMore}
+          title="Отправить"
+          color="#841584"
+        />
+      </View>
+    </SafeAreaView>
   );
-}
-
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
+export default App;
